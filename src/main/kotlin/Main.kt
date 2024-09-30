d8aebf0c3dba7a234c9cb0290804494bb91fd1d7
acunap <antonioacunaprieto@gmail.com>
2024-10-01 01:15:09 +0200

    extract URL path

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,18 @@ import java.net.ServerSocket;
 
 fun main() {
     val socket = ServerSocket(4221).accept()
-    val outputStream = socket.getOutputStream()
-    outputStream.write("HTTP/1.1 200 OK\r\n\r\n".toByteArray())
-    outputStream.close()
+
+    val input = socket.getInputStream()
+    val output = socket.getOutputStream()
+
+    input.bufferedReader().use { reader ->
+        val path = reader.readLine().split(" ")[1]
+
+        when (path) {
+            "/" -> output.write("HTTP/1.1 200 OK\r\n\r\n".toByteArray())
+            else -> output.write("HTTP/1.1 404 Not Found\r\n\r\n".toByteArray())
+        }
+    }
+
+    output.close()
 }
